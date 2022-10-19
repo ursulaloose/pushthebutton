a55cddb6835d950fcf64db73cb90cd512e0f098f
ursulaloose <113448259+ursulaloose@users.noreply.github.com>
2022-10-19 08:40:41 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    generate()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    generate()
+})
+function generate () {
+    randomPick = randint(0, 1)
+    if (randomPick == 0) {
+        mySprite.sayText("push A")
+    }
+    if (randomPick == 1) {
+        mySprite.sayText("push B")
+    }
+}
+let randomPick = 0
+let mySprite: Sprite = null
+game.splash("Push A or B as Directed")
+mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+info.startCountdown(20)
+generate()
